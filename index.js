63e2697ad62677934583de089403a6eaf96a3387
jonah-kayiwa <kayiwa.jonah@student.moringaschool.com>
2024-01-03 11:31:11 +0300

    ##destructive##

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+//solutin 1//
+function updateEmployeeWithKeyAndValue(employee, key, value) 
+{
+    return {
+      ...employee,
+      [key]: value
+    };
+}
+
+//solution 2//
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) 
+{
+    employee[key] = value;
+    return employee; 
+}
+
+//solution 3//
+function deleteFromEmployeeByKey(employee, key) 
+{
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+  
+//solution 4//
+function destructivelyDeleteFromEmployeeByKey(employee, key) 
+{
+    delete employee[key];
+    return employee; 
+}
